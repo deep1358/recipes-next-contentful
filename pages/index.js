356951b9b0f5bf3f0e66bae70977b549fd0c4c06
Deep Shah <deepshah1358@gmail.com>
2021-04-13 11:37:56 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import { createClient } from "contentful";
+import Recipecard from "../components/Recipecard";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+  const res = await client.getEntries({
+    content_type: "recipe",
+  });
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <Recipecard key={recipe.sys.id} recipe={recipe} />
+      ))}
+      <style jsx>
+        {`
+          .recipe-list {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 20px 60px;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
---
+++ pages/recipes/[slug].js
@@ -0,0 +1,104 @@
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { createClient } from "contentful";
+import Image from "next/image";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
+
+export default function RecipeDetails({ recipe }) {
+  // console.log(recipe);
+  const {
+    featuerdImage,
+    title,
+    cookingTime,
+    ingrediants,
+    method,
+  } = recipe.fields;
+  return (
+    <div>
+      <div className="banner">
+        <Image
+          src={"https:" + featuerdImage.fields.file.url}
+          width={featuerdImage.fields.file.details.image.width}
+          height={featuerdImage.fields.file.details.image.height}
+        />
+        <h2>{title}</h2>
+      </div>
+      <div className="info">
+        <p>Take about {cookingTime} mins to cook.</p>
+        <h3>Inredients: </h3>
+        {ingrediants.map((i) => (
+          <span key={i}>{i}</span>
+        ))}
+      </div>
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+
+      <style jsx>
+        {`
+          h2,
+          h3 {
+            text-transform: uppercase;
+          }
+          .banner h2 {
+            margin: 0;
+            background: #fff;
+            display: inline-block;
+            padding: 20px;
+            position: relative;
+            top: -60px;
+            left: -10px;
+            transform: rotateZ(-1deg);
+            box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          }
+          .info p {
+            margin: 0;
+          }
+          .info span::after {
+            content: ", ";
+          }
+          .info span:last-child::after {
+            content: ".";
+          }
+        `}
+      </style>
+    </div>
+  );
+}
+
+export const getStaticProps = async ({ params }) => {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+  // console.log(items);
+
+  return {
+    props: {
+      recipe: items[0],
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "recipe",
+  });
+  // console.log(res.items);
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+  // console.log(paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
